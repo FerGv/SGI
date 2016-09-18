361e1c96e22d958a259257c9de7901cbcb4f4c47
Fernando Garcia <fkngv_10@hotmail.com>
2016-09-18 00:47:06 -0500

    Organización de archivos
    - Eliminar carpeta "Cafetería"
    - Crear carpeta "Manuales" y "Query"

--- Cafeteria/botones.php
+++
@@ -1,7 +0,0 @@
-<?php 
-
-	if ($_POST[materias_primas]) { header('Location: Materias_Primas/tabla.php'); }
-
-	if ($_POST[productos_terminados]) { header('Location: Productos_Terminados/tabla.php'); }
-
-?>
---
+++ botones.php
@@ -0,0 +1,7 @@
+<?php 
+
+	if ($_POST[materias_primas]) { header('Location: Materias_Primas/tabla.php'); }
+
+	if ($_POST[productos_terminados]) { header('Location: Productos_Terminados/tabla.php'); }
+
+?>
